8cfc3b3f4ca1f12315dcb1d7e184f6c7246b24f3
Scott Tibbo <40707785+DarthWHO@users.noreply.github.com>
2025-12-16 12:54:50 -0700

    initial reducer and action creators completed

---
+++ src/store/store.jsx
@@ -0,0 +1,63 @@
+import { createStore } from "redux";
+
+const initialState = {
+  balance: 0,
+  loan: 0,
+  loanPurpose: "",
+};
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case "account/deposit":
+      return { ...state, balance: state.balance + action.payload };
+    case "account/withdraw":
+      return { ...state, balance: state.balance - action.payload };
+    case "account/requestLoan":
+      if (state.loan > 0) return state;
+      return {
+        ...state,
+        balance: state.balance + action.payload.amount,
+        loan: state.loan + action.payload.amount,
+        loanPurpose: action.payload.purpose,
+      };
+    case "account/payLoan":
+      return {
+        ...state,
+        loan: 0,
+        loanPurpose: "",
+        balance: state.balance - state.loan,
+      };
+    default:
+      return state;
+  }
+}
+
+function deposit(amount) {
+  return { type: "account/deposit", payload: amount };
+}
+function withdraw(amount) {
+  return { type: "account/withdraw", payload: amount };
+}
+function requestLoan(amount, purpose) {
+  return {
+    type: "account/requestLoan",
+    payload: { amount, purpose },
+  };
+}
+function payLoan() {
+  return { type: "account/payLoan" };
+}
+
+const store = createStore(reducer);
+
+store.dispatch(deposit(500));
+console.log(store.getState());
+
+store.dispatch(withdraw(200));
+console.log(store.getState());
+
+store.dispatch(requestLoan(1000, "get a dog"));
+console.log(store.getState());
+
+store.dispatch(payLoan());
+console.log(store.getState());
